b1a9e88b25d0eb2845a76ec88b45a8d3eb561d1f
Andrej Cizmarik <cizmarik.andrej@gmail.com>
2022-06-03 21:56:54 +0200

    Added more utility methods to `Guard`

--- src/SharpDetect.Common/Exceptions/Guard.cs
+++ src/SharpDetect.Common/Exceptions/Guard.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections;
+using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using static SharpDetect.Common.Exceptions.Guard;
 
 namespace SharpDetect.Common.Exceptions
 {
@@ -12,11 +14,11 @@ namespace SharpDetect.Common.Exceptions
         {
             var builder = new Dictionary<Type, Func<string, Exception>>()
             {
-                { typeof(ArgumentException), m => new ArgumentException(m) },
-                { typeof(ArgumentNullException), m => new ArgumentNullException(m) },
-                { typeof(ShadowRuntimeStateException), m => new ShadowRuntimeStateException(m) },
-                { typeof(InvalidOperationException), m => new InvalidOperationException(m) },
-                { typeof(InvalidProgramException), m => new InvalidProgramException(m) }
+                { typeof(ArgumentException), static m => new ArgumentException(m) },
+                { typeof(ArgumentNullException), static m => new ArgumentNullException(m) },
+                { typeof(ShadowRuntimeStateException), static m => new ShadowRuntimeStateException(m) },
+                { typeof(InvalidOperationException), static m => new InvalidOperationException(m) },
+                { typeof(InvalidProgramException), static m => new InvalidProgramException(m) }
             };
             lookup = builder.ToImmutableDictionary();
         }
@@ -28,10 +30,50 @@ namespace SharpDetect.Common.Exceptions
                 Throw<TException>($"Provided argument {expr} was evaluated to {actual}, which is not equal to {expected}.");
         }
 
+        public static GuardGreaterThanComparer<TValue, TException> Greater<TValue, TException>(TValue actual, [CallerArgumentExpression("actual")] string? expr = null)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            return new GuardGreaterThanComparer<TValue, TException>(actual, expr!);
+        }
+
+        public static GuardGreaterOrEqualThanComparer<TValue, TException> GreaterOrEqual<TValue, TException>(TValue actual, [CallerArgumentExpression("actual")] string? expr = null)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            return new GuardGreaterOrEqualThanComparer<TValue, TException>(actual, expr!);
+        }
+
+        public static GuardLessThanComparer<TValue, TException> Less<TValue, TException>(TValue actual, [CallerArgumentExpression("actual")] string? expr = null)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            return new GuardLessThanComparer<TValue, TException>(actual, expr!);
+        }
+
+        public static GuardLessOrEqualThanComparer<TValue, TException> LessOrEqual<TValue, TException>(TValue actual, [CallerArgumentExpression("actual")] string? expr = null)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            return new GuardLessOrEqualThanComparer<TValue, TException>(actual, expr!);
+        }
+
         public static void NotEqual<TValue, TException>(TValue invalid, TValue actual, [CallerArgumentExpression("actual")] string? expr = null)
         {
             if (EqualityComparer<TValue>.Default.Equals(invalid, actual))
                 Throw<TException>($"Provided argument {expr} was evaluated to {actual}, which was equal to {invalid}.");
+        }
+
+        public static void NotEmpty<TException>(ICollection collection, [CallerArgumentExpression("collection")] string? expr = null)
+        {
+            if (collection.Count == 0)
+                Throw<TException>($"Provided collection {expr} was empty.");
+        }
+
+        public static void Single<TException>(ICollection collection, [CallerArgumentExpression("collection")] string? expr = null)
+        {
+            if (collection.Count != 1)
+                Throw<TException>($"Provided collection {expr} does not have a single element.");
         }
 
         public static TValue NotNull<TValue, TException>([NotNull] TValue? value, [CallerArgumentExpression("value")] string? expr = null)
@@ -59,9 +101,49 @@ namespace SharpDetect.Common.Exceptions
         }
 
         [DoesNotReturn]
-        private static void Throw<TException>(string? message)
+        internal static void Throw<TException>(string? message)
         {
             throw lookup[typeof(TException)](message!);
+        }
+
+        public record struct GuardGreaterThanComparer<TValue, TException>(TValue Value, string Expression);
+        public record struct GuardLessThanComparer<TValue, TException>(TValue Value, string Expression);
+        public record struct GuardGreaterOrEqualThanComparer<TValue, TException>(TValue Value, string Expression);
+        public record struct GuardLessOrEqualThanComparer<TValue, TException>(TValue Value, string Expression);
+    }
+
+    public static class GuardCompareExtensions
+    {
+        public static void Than<TValue, TException>(this GuardGreaterThanComparer<TValue, TException> left, TValue right)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            if (left.Value.CompareTo(right) <= 0)
+                Throw<TException>($"Provided argument {left.Expression} was not greater than {right}.");
+        }
+
+        public static void Than<TValue, TException>(this GuardGreaterOrEqualThanComparer<TValue, TException> left, TValue right)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            if (left.Value.CompareTo(right) < 0)
+                Throw<TException>($"Provided argument {left.Expression} was not greater or equal than {right}.");
+        }
+
+        public static void Than<TValue, TException>(this GuardLessThanComparer<TValue, TException> left, TValue right)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            if (left.Value.CompareTo(right) >= 0)
+                Throw<TException>($"Provided argument {left.Expression} was not lesser than {right}.");
+        }
+
+        public static void Than<TValue, TException>(this GuardLessOrEqualThanComparer<TValue, TException> left, TValue right)
+            where TException : Exception
+            where TValue : IComparable<TValue>
+        {
+            if (left.Value.CompareTo(right) > 0)
+                Throw<TException>($"Provided argument {left.Expression} was not lesser or equal than {right}.");
         }
     }
 }
