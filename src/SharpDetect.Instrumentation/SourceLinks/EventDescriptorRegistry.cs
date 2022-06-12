5a05268e8dfa1e2a2ef4d207ca59b5bc00d76af2
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2022-06-12 20:52:42 +0200

    Implemented initial version of `EventDescriptorRegistry`

--- src/SharpDetect.Instrumentation/SourceLinks/EventDescriptorRegistry.cs
+++ src/SharpDetect.Instrumentation/SourceLinks/EventDescriptorRegistry.cs
@@ -1,21 +1,36 @@
 ﻿using dnlib.DotNet;
 using dnlib.DotNet.Pdb;
 using SharpDetect.Common;
+using SharpDetect.Common.Exceptions;
 using SharpDetect.Common.Services.Instrumentation;
 using SharpDetect.Common.SourceLinks;
+using System.Collections.Concurrent;
 
 namespace SharpDetect.Instrumentation.SourceLinks
 {
     internal class EventDescriptorRegistry : IEventDescriptorRegistry
     {
+        private ConcurrentDictionary<ulong, SourceLink> sourceLinks;
+        private ulong idCounter;
+
+        public EventDescriptorRegistry()
+        {
+            this.sourceLinks = new ConcurrentDictionary<ulong, SourceLink>();
+            this.idCounter = 0;
+        }
+
         public SourceLink Create(AnalysisEventType type, MethodDef method, uint instructionOffset, SequencePoint? sequencePoint = null)
         {
-            throw new NotImplementedException();
+            var newId = Interlocked.Increment(ref idCounter);
+            var sourceLink = new SourceLink(newId, type, method, instructionOffset, sequencePoint);
+            Guard.True<ArgumentException>(sourceLinks.TryAdd(newId, sourceLink));
+
+            return sourceLink;
         }
 
         public SourceLink Get(ulong eventId)
         {
-            throw new NotImplementedException();
+            return sourceLinks[eventId];
         }
     }
 }
