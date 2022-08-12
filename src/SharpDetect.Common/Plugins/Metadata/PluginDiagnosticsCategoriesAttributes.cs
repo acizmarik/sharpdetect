00ef4be0b99e91fb84fa60b633c2b1576a2bca4b
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2022-08-12 18:14:35 +0200

    Added metadata entries for plugins to specify reporting categories

---
+++ src/SharpDetect.Common/Plugins/Metadata/PluginDiagnosticsCategoriesAttributes.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpDetect.Common.Plugins.Metadata
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PluginDiagnosticsCategoriesAttribute : Attribute
+    {
+        public readonly string[] Categories;
+
+        public PluginDiagnosticsCategoriesAttribute(string[] categories)
+        {
+            Categories = categories;
+        }
+    }
+}
