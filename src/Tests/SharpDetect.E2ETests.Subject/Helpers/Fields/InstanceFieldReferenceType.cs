70b51953a45c715289086a433c331269bf073405
Andrej Cizmarik <cizmarik.andrej@gmail.com>
2022-09-02 00:58:51 +0200

    Added E2E tests for basic field read events

---
+++ src/Tests/SharpDetect.E2ETests.Subject/Helpers/Fields/InstanceFieldReferenceType.cs
@@ -0,0 +1,7 @@
+﻿namespace SharpDetect.E2ETests.Subject.Helpers.Fields
+{
+    public class InstanceFieldReferenceType
+    {
+        public object? Test_Field_ReferenceType_Instance;
+    }
+}
--- src/Tests/SharpDetect.E2ETests.Subject/Program.cs
+++ src/Tests/SharpDetect.E2ETests.Subject/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SharpDetect.E2ETests.Subject
+﻿using SharpDetect.E2ETests.Subject.Helpers.Fields;
+
+namespace SharpDetect.E2ETests.Subject
 {
     public static class Program
     {
@@ -37,6 +39,48 @@
                 Monitor.Exit(obj);
         }
 
+        public static void Test_Field_ValueType_Instance_Read()
+        {
+            var cpy = new InstanceFieldValueType().Test_Field_ValueType_Instance;
+        }
+
+        public static void Test_Field_ValueType_Instance_Write()
+        {
+            var instance = new InstanceFieldValueType();
+            instance.Test_Field_ValueType_Instance = 123;
+        }
+
+        public static void Test_Field_ReferenceType_Instance_Read()
+        {
+            var cpy = new InstanceFieldReferenceType().Test_Field_ReferenceType_Instance;
+        }
+
+        public static void Test_Field_ReferenceType_Instance_Write()
+        {
+            var instance = new InstanceFieldReferenceType();
+            instance.Test_Field_ReferenceType_Instance = new object();
+        }
+
+        public static void Test_Field_ValueType_Static_Read()
+        {
+            var cpy = StaticFieldValueType.Test_Field_ValueType_Static;
+        }
+
+        public static void Test_Field_ValueType_Static_Write()
+        {
+            StaticFieldValueType.Test_Field_ValueType_Static = 123;
+        }
+
+        public static void Test_Field_ReferenceType_Static_Read()
+        {
+            var cpy = StaticFieldReferenceType.Test_Field_ReferenceType_Static;
+        }
+
+        public static void Test_Field_ReferenceType_Static_Write()
+        {
+            StaticFieldReferenceType.Test_Field_ReferenceType_Static = new object();
+        }
+
 
         public static void Main(string[] args)
         {
@@ -44,6 +88,7 @@
 
             switch (args[0])
             {
+                // Locks
                 case nameof(Test_MonitorMethods_EnterExit1):
                     Test_MonitorMethods_EnterExit1();
                     break;
@@ -51,13 +96,38 @@
                     Test_MonitorMethods_EnterExit2();
                     break;
                 case nameof(Test_MonitorMethods_TryEnterExit1):
-                    Test_MonitorMethods_TryEnterExit1();
+                    Test_MonitorMethods_TryEnterExit1 ();
                     break;
                 case nameof(Test_MonitorMethods_TryEnterExit2):
                     Test_MonitorMethods_TryEnterExit2();
                     break;
                 case nameof(Test_MonitorMethods_TryEnterExit3):
                     Test_MonitorMethods_TryEnterExit3();
+                    break;
+                // Fields
+                case nameof(Test_Field_ValueType_Instance_Read):
+                    Test_Field_ValueType_Instance_Read();
+                    break;
+                case nameof(Test_Field_ValueType_Instance_Write):
+                    Test_Field_ValueType_Instance_Write();
+                    break;
+                case nameof(Test_Field_ReferenceType_Instance_Read):
+                    Test_Field_ReferenceType_Instance_Read();
+                    break;
+                case nameof(Test_Field_ReferenceType_Instance_Write):
+                    Test_Field_ReferenceType_Instance_Write();
+                    break;
+                case nameof(Test_Field_ValueType_Static_Read):
+                    Test_Field_ValueType_Static_Read();
+                    break;
+                case nameof(Test_Field_ValueType_Static_Write):
+                    Test_Field_ValueType_Static_Write();
+                    break;
+                case nameof(Test_Field_ReferenceType_Static_Read):
+                    Test_Field_ReferenceType_Static_Read();
+                    break;
+                case nameof(Test_Field_ReferenceType_Static_Write):
+                    Test_Field_ReferenceType_Static_Write();
                     break;
             }
         }
