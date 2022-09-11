6a208ed9be7d349e92173b91ebe8617da93e7f53
Andrej Cizmarik <cizmarik.andrej@gmail.com>
2022-09-12 00:19:48 +0200

    Fixed issue with `OperationContext` persisting information from previous notifications

--- src/SharpDetect.Core/Runtime/Threads/OperationContext.cs
+++ src/SharpDetect.Core/Runtime/Threads/OperationContext.cs
@@ -2,8 +2,43 @@
 {
     internal class OperationContext
     {
-        public ShadowObject? FieldInstance { get; set; }
-        public ShadowObject? ArrayInstance { get; set; }
-        public int? ArrayIndex { get; set; }
+        private ShadowObject? fieldInstance, arrayInstance;
+        private int? arrayIndex;
+
+        public ShadowObject? GetAndResetLastFieldInstance()
+        {
+            var result = fieldInstance;
+            fieldInstance = null;
+            return result;
+        }
+
+        public void SetFieldInstance(ShadowObject? instance)
+        {
+            fieldInstance = instance;
+        }
+
+        public ShadowObject? GetAndResetLastArrayInstance()
+        {
+            var result = arrayInstance;
+            arrayInstance = null;
+            return result;
+        }
+
+        public void SetArrayInstance(ShadowObject? instance)
+        {
+            arrayInstance = instance;
+        }
+
+        public int? GetAndResetLastArrayIndex()
+        {
+            var result = arrayIndex;
+            arrayIndex = null;
+            return result;
+        }
+
+        public void SetArrayIndex(int? index)
+        {
+            arrayIndex = index;
+        }
     }
 }
