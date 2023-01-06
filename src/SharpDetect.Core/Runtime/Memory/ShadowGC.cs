5758d175f564d0501ed7d8a728320bcd98456725
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2023-01-06 14:53:52 +0100

    Fixed objects promotion bug in GC events

--- src/SharpDetect.Core/Runtime/Memory/ShadowGC.cs
+++ src/SharpDetect.Core/Runtime/Memory/ShadowGC.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using SharpDetect.Common.Interop;
+using SharpDetect.Common.Runtime;
 using GcGenerationRange = SharpDetect.Common.Interop.COR_PRF_GC_GENERATION_RANGE;
 
 namespace SharpDetect.Core.Runtime.Memory
@@ -7,23 +9,37 @@ namespace SharpDetect.Core.Runtime.Memory
     {
         public volatile int GarbageCollectionsCount = 0;
         private readonly ShadowMemory memory;
+        private readonly ILogger logger;
 
         public ShadowGC(ILoggerFactory loggerFactory)
         {
             memory = new(loggerFactory);
+            logger = loggerFactory.CreateLogger<ShadowGC>();
         }
 
         public void ProcessGarbageCollectionStarted(GcGenerationRange[] ranges, bool[] generationsCollected)
         {
+            logger.LogDebug("GC started for generations {gens}", generationsCollected);
+            memory.PrepareForGarbageCollection(generationsCollected);
             memory.Reconstruct(ranges);
-            memory.StartGarbageCollection(generationsCollected);
+            memory.StartGarbageCollection();
         }
 
         public void ProcessGarbageCollectionFinished(GcGenerationRange[] ranges)
         {
             memory.FinishGarbageCollection();
-            memory.PromoteSurvivors();
             memory.Reconstruct(ranges);
+            memory.PromoteSurvivors();
+            memory.TearDownGarbageCollection();
+
+            var gen0Size = memory.GetGenerationSize(COR_PRF_GC_GENERATION.COR_PRF_GC_GEN_0);
+            var gen1Size = memory.GetGenerationSize(COR_PRF_GC_GENERATION.COR_PRF_GC_GEN_1);
+            var gen2Size = memory.GetGenerationSize(COR_PRF_GC_GENERATION.COR_PRF_GC_GEN_2);
+            var lohSize = memory.GetGenerationSize(COR_PRF_GC_GENERATION.COR_PRF_GC_LARGE_OBJECT_HEAP);
+            var pohSize = memory.GetGenerationSize(COR_PRF_GC_GENERATION.COR_PRF_GC_PINNED_OBJECT_HEAP);
+
+            logger.LogDebug("GC finished. Live & tracked objects: [G0={g0},G1={g1},G2={g2},LOH={loh},POH={poh}]", 
+                gen0Size, gen1Size, gen2Size, lohSize, pohSize);
         }
 
         public void ProcessSurvivingReferences(UIntPtr[] survivingBlockStarts, uint[] lengths)
@@ -40,5 +56,8 @@ namespace SharpDetect.Core.Runtime.Memory
 
         public ShadowObject GetObject(UIntPtr pointer)
             => memory.GetOrTrack(pointer);
+
+        public COR_PRF_GC_GENERATION GetObjectGeneration(IShadowObject obj)
+            => memory.GetGeneration(obj.ShadowPointer);
     }
 }
