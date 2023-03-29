9302262ee90c196dd1e4e26a105da7ad1ba47824
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2023-03-29 19:51:42 +0200

    Add missing license header

--- src/SharpDetect.Core/IAnalysis.cs
+++ src/SharpDetect.Core/IAnalysis.cs
@@ -1,4 +1,7 @@
-﻿namespace SharpDetect.Core
+﻿// Copyright 2023 Andrej Čižmárik and Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace SharpDetect.Core
 {
     public interface IAnalysis
     {
--- src/SharpDetect.Core/Target.cs
+++ src/SharpDetect.Core/Target.cs
@@ -1,4 +1,7 @@
-﻿using CliWrap;
+﻿// Copyright 2023 Andrej Čižmárik and Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+using CliWrap;
 using CommunityToolkit.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
