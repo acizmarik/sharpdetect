21cafbfd83052ec1a5870e1a1b3181df5bb27d63
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2025-11-18 22:32:22 +0100

    Refactor E2E tests using temporal asserts

---
+++ src/Tests/SharpDetect.E2ETests/EventMatchers.cs
@@ -0,0 +1,42 @@
+// Copyright 2025 Andrej Čižmárik and Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+using SharpDetect.Core.Events;
+using SharpDetect.E2ETests.Utils;
+using SharpDetect.TemporalAsserts;
+
+namespace SharpDetect.E2ETests;
+
+public static class EventMatchers
+{
+    public static AtomicPredicate<ulong, RecordedEventType> EventType(RecordedEventType type) =>
+        new(evt => evt.Type == type);
+
+    public static AtomicPredicate<ulong, RecordedEventType> MethodEnter(string methodName, TestHappensBeforePlugin plugin)
+    {
+        return new AtomicPredicate<ulong, RecordedEventType>(
+            evt =>
+            {
+                if (evt.Type != RecordedEventType.MethodEnter)
+                    return false;
+
+                var (metadata, args) = evt.Get<(RecordedEventMetadata, MethodEnterRecordedEvent)>();
+                var method = plugin.Resolve(metadata, args.ModuleId, args.MethodToken);
+                return method.Name.StartsWith(methodName);
+            });
+    }
+
+    public static AtomicPredicate<ulong, RecordedEventType> MethodExit(string methodName, TestHappensBeforePlugin plugin)
+    {
+        return new AtomicPredicate<ulong, RecordedEventType>(
+            evt =>
+            {
+                if (evt.Type != RecordedEventType.MethodExit)
+                    return false;
+
+                var (metadata, args) = evt.Get<(RecordedEventMetadata, MethodExitRecordedEvent)>();
+                var method = plugin.Resolve(metadata, args.ModuleId, args.MethodToken);
+                return method.Name.StartsWith(methodName);
+            });
+    }
+}
--- src/Tests/SharpDetect.E2ETests/MethodInterpretationTests.cs
+++ src/Tests/SharpDetect.E2ETests/MethodInterpretationTests.cs
@@ -1,14 +1,16 @@
 // Copyright 2025 Andrej Čižmárik and Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-using dnlib.DotNet;
 using Microsoft.Extensions.DependencyInjection;
 using SharpDetect.Core.Events;
-using SharpDetect.Core.Plugins.Models;
 using SharpDetect.E2ETests.Utils;
+using SharpDetect.TemporalAsserts;
+using SharpDetect.TemporalAsserts.TemporalOperators;
 using SharpDetect.Worker;
+using SharpDetect.Worker.Commands.Run;
 using Xunit;
 using Xunit.Abstractions;
+using static SharpDetect.E2ETests.TemporalAssertionBuilders;
 
 namespace SharpDetect.E2ETests;
 
@@ -20,233 +22,39 @@ public class MethodInterpretationTests(ITestOutputHelper testOutput)
     [Theory]
 #if DEBUG
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_EnterExit1_Debug.json")]
-#elif RELEASE
-    [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_EnterExit1_Release.json")]
-#endif
-    public async Task MethodInterpretation_Monitor_EnterExit_LockStatement(string configuration)
-    {
-        // Arrange
-        using var services = TestContextFactory.CreateServiceProvider(configuration, testOutput);
-        var plugin = services.GetRequiredService<TestHappensBeforePlugin>();
-        var analysisWorker = services.GetRequiredService<IAnalysisWorker>();
-        var enteredTest = false;
-        var exitedTest = false;
-        var insideTestMethod = false;
-        var methods = new List<MethodDef>();
-        var lockObjects = new HashSet<Lock>();
-        plugin.MethodEntered += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = true;
-                enteredTest = true;
-            }
-        };
-        plugin.MethodExited += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = false;
-                exitedTest = true;
-            }
-        };
-        plugin.LockAcquireAttempted += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-        plugin.LockAcquireReturned += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-        plugin.LockReleased += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-
-        // Execute
-        await analysisWorker.ExecuteAsync(CancellationToken.None);
-
-        // Assert
-        Assert.True(enteredTest);
-        Assert.True(exitedTest);
-        Assert.Single(lockObjects);
-        Assert.Equivalent(new List<string>
-        {
-            "System.Void System.Threading.Monitor::ReliableEnter(System.Object,System.Boolean&)",
-            "System.Void System.Threading.Monitor::ReliableEnter(System.Object,System.Boolean&)",
-            "System.Void System.Threading.Monitor::Exit(System.Object)",
-        }, methods.Select(m => m.FullName));
-    }
-
-    [Theory]
-#if DEBUG
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_EnterExit2_Debug.json")]
-#elif RELEASE
-    [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_EnterExit2_Release.json")]
-#endif
-    public async Task MethodInterpretation_Monitor_EnterExit_Unsafe(string configuration)
-    {
-        // Arrange
-        using var services = TestContextFactory.CreateServiceProvider(configuration, testOutput);
-        var plugin = services.GetRequiredService<TestHappensBeforePlugin>();
-        var analysisWorker = services.GetRequiredService<IAnalysisWorker>();
-        var enteredTest = false;
-        var exitedTest = false;
-        var insideTestMethod = false;
-        var methods = new List<MethodDef>();
-        var lockObjects = new HashSet<Lock>();
-        plugin.MethodEntered += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = true;
-                enteredTest = true;
-            }
-        };
-        plugin.MethodExited += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = false;
-                exitedTest = true;
-            }
-        };
-        plugin.LockAcquireAttempted += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-        plugin.LockAcquireReturned += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-        plugin.LockReleased += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-
-        // Execute
-        await analysisWorker.ExecuteAsync(CancellationToken.None);
-
-        // Assert
-        Assert.True(enteredTest);
-        Assert.True(exitedTest);
-        Assert.Single(lockObjects);
-        Assert.Equivalent(new List<string>
-        {
-            "System.Void System.Threading.Monitor::Enter(System.Object)",
-            "System.Void System.Threading.Monitor::Enter(System.Object)",
-            "System.Void System.Threading.Monitor::Exit(System.Object)",
-        }, methods.Select(m => m.FullName));
-    }
-
-    [Theory]
-#if DEBUG
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_TryEnterExit1_Debug.json")]
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_TryEnterExit2_Debug.json")]
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_TryEnterExit3_Debug.json")]
 #elif RELEASE
+    [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_EnterExit1_Release.json")]
+    [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_EnterExit2_Release.json")]
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_TryEnterExit1_Release.json")]
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_TryEnterExit2_Release.json")]
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Monitor_TryEnterExit3_Release.json")]
 #endif
-    public async Task MethodInterpretation_Monitor_TryEnterExit(string configuration)
+    public async Task MethodInterpretation_Monitor_EnterExit(string configuration)
     {
         // Arrange
         using var services = TestContextFactory.CreateServiceProvider(configuration, testOutput);
+        var args = services.GetRequiredService<RunCommandArgs>();
         var plugin = services.GetRequiredService<TestHappensBeforePlugin>();
         var analysisWorker = services.GetRequiredService<IAnalysisWorker>();
-        var enteredTest = false;
-        var exitedTest = false;
-        var insideTestMethod = false;
-        var methods = new List<MethodDef>();
-        var lockObjects = new HashSet<Lock>();
-        plugin.MethodEntered += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = true;
-                enteredTest = true;
-            }
-        };
-        plugin.MethodExited += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = false;
-                exitedTest = true;
-            }
-        };
-        plugin.LockAcquireAttempted += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-        plugin.LockAcquireReturned += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
-        plugin.LockReleased += e =>
-        {
-            if (insideTestMethod)
-            {
-                methods.Add(plugin.Resolve(new RecordedEventMetadata(e.ProcessId, e.ThreadId), e.ModuleId, e.MethodToken));
-                lockObjects.Add(e.LockObj);
-            }
-        };
+        var events = new TestEventsEnumerable(plugin);
+        var assert = EventuallyMethodEnter(args.Target.Args!, plugin)
+            .Then(EventuallyEventType(RecordedEventType.MonitorLockAcquire))
+            .Then(EventuallyEventType(RecordedEventType.MonitorLockAcquireResult))
+            .Then(EventuallyEventType(RecordedEventType.MonitorLockRelease))
+            .Then(EventuallyMethodExit(args.Target.Args!, plugin));
 
         // Execute
         await analysisWorker.ExecuteAsync(CancellationToken.None);
 
         // Assert
-        Assert.True(enteredTest);
-        Assert.True(exitedTest);
-        Assert.Single(lockObjects);
-        Assert.Equivalent(new List<string>
-        {
-            "System.Void System.Threading.Monitor::ReliableEnterTimeout(System.Object,System.Int32,System.Boolean&)",
-            "System.Void System.Threading.Monitor::ReliableEnterTimeout(System.Object,System.Int32,System.Boolean&)",
-            "System.Void System.Threading.Monitor::Exit(System.Object)",
-        }, methods.Select(m => m.FullName));
+        Assert.Equal(AssertStatus.Satisfied, assert.Evaluate(events));
     }
     
-        [Theory]
+    [Theory]
 #if DEBUG
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Thread_Join1_Debug.json")]
     [InlineData($"{ConfigurationFolder}/MethodInterpretation_Thread_Join2_Debug.json")]
@@ -260,49 +68,19 @@ public class MethodInterpretationTests(ITestOutputHelper testOutput)
     {
         // Arrange
         using var services = TestContextFactory.CreateServiceProvider(configuration, testOutput);
+        var args = services.GetRequiredService<RunCommandArgs>();
         var plugin = services.GetRequiredService<TestHappensBeforePlugin>();
         var analysisWorker = services.GetRequiredService<IAnalysisWorker>();
-        var enteredTest = false;
-        var exitedTest = false;
-        var joinAttempted = false;
-        var joinReturned = false;
-        var insideTestMethod = false;
-        plugin.MethodEntered += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = true;
-                enteredTest = true;
-            }
-        };
-        plugin.MethodExited += e =>
-        {
-            var method = plugin.Resolve(e.Metadata, e.Args.ModuleId, e.Args.MethodToken);
-            if (method.Name.StartsWith("Test_"))
-            {
-                insideTestMethod = false;
-                exitedTest = true;
-            }
-        };
-        plugin.ThreadJoinAttempted += e =>
-        {
-            if (insideTestMethod)
-                joinAttempted = true;
-        };
-        plugin.LockAcquireReturned += e =>
-        {
-            if (insideTestMethod)
-                joinReturned = true;
-        };
+        var events = new TestEventsEnumerable(plugin);
+        var assert = EventuallyMethodEnter(args.Target.Args!, plugin)
+            .Then(EventuallyEventType(RecordedEventType.ThreadJoinAttempt))
+            .Then(EventuallyEventType(RecordedEventType.ThreadJoinResult))
+            .Then(EventuallyMethodExit(args.Target.Args!, plugin));
 
         // Execute
         await analysisWorker.ExecuteAsync(CancellationToken.None);
 
         // Assert
-        Assert.True(enteredTest);
-        Assert.True(exitedTest);
-        Assert.True(joinAttempted);
-        Assert.True(joinReturned);
+        Assert.Equal(AssertStatus.Satisfied, assert.Evaluate(events));
     }
 }
