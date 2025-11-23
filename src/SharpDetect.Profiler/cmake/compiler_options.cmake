030f5213d48fc96fcc8e734b09c62776f15f811c
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2025-11-23 22:37:07 +0100

    Fix static analysis violations for CMake

--- src/SharpDetect.Profiler/cmake/compiler_options.cmake
+++ src/SharpDetect.Profiler/cmake/compiler_options.cmake
@@ -3,8 +3,23 @@ set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 if (NOT (CMAKE_SIZEOF_VOID_P EQUAL 8))
-    message(FATAL_ERROR "Unsupported architecture. Expected 64 bit." )
+    message(FATAL_ERROR "Unsupported architecture. Expected 64 bit.")
 endif()
+
+function(apply_profiler_compile_options target_name)
+    if (UNIX AND NOT APPLE)
+        target_compile_options(${target_name} PRIVATE
+            -g
+            -fPIC
+            -fms-extensions
+            -Wno-pragma-pack)
+        target_compile_definitions(${target_name} PRIVATE
+            HOST_AMD64
+            HOST_64BIT
+            PLATFORM_UNIX
+            PAL_STDCPP_COMPAT)
+    endif()
+endfunction()
 
 if (WIN32)
     message("Windows x64 ${CMAKE_BUILD_TYPE} build")
@@ -12,18 +27,8 @@ if (WIN32)
 elseif (UNIX AND NOT APPLE)
     message("Linux x64 ${CMAKE_BUILD_TYPE} build")
     enable_language(ASM)
-    add_compile_options(
-        -g
-        -fPIC 
-        -fms-extensions 
-        -Wno-pragma-pack
-        -DHOST_AMD64 
-        -DHOST_64BIT 
-        -DPLATFORM_UNIX
-        -DPAL_STDCPP_COMPAT)
     set(CMAKE_STATIC_LIBRARY_PREFIX "")
     set(CMAKE_SHARED_LIBRARY_PREFIX "")
-
 else()
-    message(FATAL_ERROR "Unsupported platform. Expected Windows or Linux." )
+    message(FATAL_ERROR "Unsupported platform. Expected Windows or Linux.")
 endif()
