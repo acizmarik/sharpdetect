e17c97998319bbee2f421a82a510d396a7059532
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2025-12-06 17:20:52 +0100

    Simplify shadow runtime consistency tests

--- src/Tests/SharpDetect.E2ETests/ShadowCallstackTests.cs
+++ src/Tests/SharpDetect.E2ETests/ShadowCallstackTests.cs
@@ -2,15 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using Microsoft.Extensions.DependencyInjection;
-using SharpDetect.Core.Events;
-using SharpDetect.E2ETests.Utils;
-using SharpDetect.TemporalAsserts;
-using SharpDetect.TemporalAsserts.TemporalOperators;
 using SharpDetect.Worker;
-using SharpDetect.Worker.Commands.Run;
 using Xunit;
 using Xunit.Abstractions;
-using static SharpDetect.E2ETests.TemporalAssertionBuilders;
 
 namespace SharpDetect.E2ETests;
 
@@ -29,23 +23,13 @@ public class ShadowCallstackTests(ITestOutputHelper testOutput)
     {
         // Arrange
         using var services = TestContextFactory.CreateServiceProvider(configuration, testOutput);
-        var args = services.GetRequiredService<RunCommandArgs>();
-        var plugin = services.GetRequiredService<TestHappensBeforePlugin>();
         var analysisWorker = services.GetRequiredService<IAnalysisWorker>();
-        var events = new TestEventsEnumerable(plugin);
-        var assert = EventuallyMethodEnter(args.Target.Args!, plugin)
-            .Then(EventuallyEventType(RecordedEventType.MonitorLockAcquire))
-            .Then(EventuallyEventType(RecordedEventType.MonitorLockAcquireResult))
-            .Then(EventuallyEventType(RecordedEventType.MonitorWaitAttempt))
-            .Then(EventuallyEventType(RecordedEventType.MonitorWaitResult))
-            .Then(EventuallyEventType(RecordedEventType.MonitorLockRelease))
-            .Then(EventuallyMethodExit(args.Target.Args!, plugin));
 
         // Execute
-        await analysisWorker.ExecuteAsync(CancellationToken.None);
+        var exception = Record.ExceptionAsync(async () => await analysisWorker.ExecuteAsync(CancellationToken.None));
 
         // Assert
-        Assert.True(AssertStatus.Satisfied == assert.Evaluate(events), assert.GetDiagnosticInfo());
+        Assert.Null(await exception);
     }
 }
 
