f9f1bab09fcf145a03bbad3773a2fed358b047de
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2026-01-05 00:11:26 +0100

    Fix out of order monitor signals processing

--- src/Extensibility/SharpDetect.PluginHost/Services/RecordedEventsDeliveryContext.cs
+++ src/Extensibility/SharpDetect.PluginHost/Services/RecordedEventsDeliveryContext.cs
@@ -12,6 +12,7 @@ internal class RecordedEventsDeliveryContext : IRecordedEventsDeliveryContext
 {
     private readonly Dictionary<ProcessThreadId, Queue<RecordedEvent>> _undelivered;
     private readonly Dictionary<ShadowLock, Queue<ProcessThreadId>> _waitForLockQueue;
+    private readonly Dictionary<ShadowLock, List<ProcessThreadId>> _waitForSignalQueue;
     private readonly Dictionary<ProcessTrackedObjectId, Queue<ProcessThreadId>> _waitForThreadStartQueue;
     private readonly HashSet<ProcessThreadId> _blockedThreads;
     private readonly HashSet<ProcessThreadId> _unblockedThreads;
@@ -19,10 +20,26 @@ internal class RecordedEventsDeliveryContext : IRecordedEventsDeliveryContext
     public RecordedEventsDeliveryContext()
     {
         _undelivered = [];
+        _waitForSignalQueue = [];
         _waitForLockQueue = [];
         _waitForThreadStartQueue = [];
         _blockedThreads = [];
         _unblockedThreads = [];
+    }
+
+    public void RegisterThreadWaitingForObjectPulse(ProcessThreadId processThreadId, ShadowLock lockObj)
+    {
+        if (!_waitForSignalQueue.ContainsKey(lockObj))
+            _waitForSignalQueue[lockObj] = new List<ProcessThreadId>();
+        _waitForSignalQueue[lockObj].Add(processThreadId);
+    }
+
+    public void UnregisterThreadWaitingForObjectPulse(ProcessThreadId processThreadId, ShadowLock lockObj)
+    {
+        if (!_waitForSignalQueue.TryGetValue(lockObj, out var queue))
+            return;
+
+        queue.Remove(processThreadId);
     }
 
     public void BlockEventsDeliveryForThreadWaitingForObject(ProcessThreadId processThreadId, ShadowLock lockObj)
@@ -45,6 +62,28 @@ internal class RecordedEventsDeliveryContext : IRecordedEventsDeliveryContext
         if (!_waitForThreadStartQueue.ContainsKey(threadObjectId))
             _waitForThreadStartQueue[threadObjectId] = new Queue<ProcessThreadId>();
         _waitForThreadStartQueue[threadObjectId].Enqueue(processThreadId);
+    }
+
+    public bool SignalOneThreadWaitingForObjectPulse(ShadowLock lockObj)
+    {
+        if (_waitForSignalQueue.TryGetValue(lockObj, out var waitQueue) && waitQueue.Count > 0)
+        {
+            waitQueue.RemoveAt(0);
+            return true;
+        }
+        
+        return false;
+    }
+
+    public bool SignalAllThreadsWaitingForObjectPulse(ShadowLock lockObj)
+    {
+        if (_waitForSignalQueue.TryGetValue(lockObj, out var waitQueue))
+        {
+            waitQueue.Clear();
+            return true;
+        }
+
+        return false;
     }
 
     public void UnblockEventsDeliveryForThreadWaitingForObject(ShadowLock lockObj)
@@ -78,6 +117,11 @@ internal class RecordedEventsDeliveryContext : IRecordedEventsDeliveryContext
     public bool HasUndeliveredEvents(ProcessThreadId threadId)
     {
         return _undelivered.ContainsKey(threadId) && _undelivered[threadId].Count > 0;
+    }
+
+    public bool IsWaitingForObjectPulse(ProcessThreadId threadId, ShadowLock lockObj)
+    {
+        return _waitForSignalQueue.TryGetValue(lockObj, out var queue) && queue.Contains(threadId);
     }
 
     public IEnumerable<RecordedEvent> ConsumeUndeliveredEvents(ProcessThreadId processThreadId)
