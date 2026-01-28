3528a3876d51cdffd430b7079a56dde68d79dea4
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2026-01-28 19:01:26 +0100

    Add SimpleDataRace sample

---
+++ src/Samples/SimpleDataRace/Program.cs
@@ -0,0 +1,26 @@
+﻿// Copyright 2026 Andrej Čižmárik and Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+Test.Field = 0;
+
+var thread1 = new Thread(() =>
+{
+    Test.Field = 1;
+});
+
+var thread2 = new Thread(() =>
+{
+    Test.Field = 2;
+});
+
+thread1.Start();
+thread2.Start();
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine(Test.Field);
+
+static class Test
+{
+    public static int Field;
+}
